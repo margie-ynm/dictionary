d926b2b485885a61dff20d9c66bb0beaa1ab9eff
Margie Nevarez <margie.ynm@gmail.com>
2016-12-02 10:39:14 -0800

    add second termlist for an attribute

--- lib/word.rb
+++ lib/word.rb
@@ -1,8 +1,9 @@
 class Word
 
-  attr_reader(:name)
+  attr_accessor(:name, :term_list)
 
   define_method(:initialize) do |attributes|
     @name = attributes.fetch(:name)
+    @term_list = []
   end
 end
--- spec/word_spec.rb
+++ spec/word_spec.rb
@@ -1,14 +1,12 @@
 require('word')
 require('rspec')
 
-require('word')
-require('rspec')
-
 describe(Word) do
   describe("#initialize") do
-    it('will initialize attribute') do
+    it('will check the initial attributes') do
       test_word = Word.new({:name => "extraordinary"})
       expect(test_word.name).to(eq('extraordinary'))
+      expect(test_word.term_list).to(eq([]))
     end
   end
 end
